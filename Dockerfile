36e403bf0aa0bc964c09e8ac00ce2ece7afca546
Christian Kniep <christian@qnib.org>
2014-08-21 22:48:16 +0200

    iniital commit

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+###### Updated version of fedora (20)
+FROM centos6/supervisor
+MAINTAINER "Christian Kniep <christian@qnib.org>"
+
+EXPOSE 80
+VOLUME ["/var/www/html", "/var/lib/mysql"]
+RUN yum install -y tar
+
+RUN yum install -y mysql-server ImageMagick-perl
+RUN yum install -y httpd php php-pear php-xml php-mysql php-intl php-pecl-apc php-gd php-mbstring
+
+# httpd start
+ADD etc/supervisor.d/httpd.ini /etc/supervisord.d/httpd.ini
+
+# Jumpstart mariadb
+RUN useradd mysql
+RUN mkdir -p /var/run/mysqld; chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
+ADD etc/supervisor.d/start_mysqld.ini /etc/supervisord.d/start_mysqld.ini
+ADD root/bin/start_mysqld.sh /root/bin/start_mysqld.sh
+
+ADD collabtive20_init.tar /root/collabtive20_init.tar
+ADD backup_init.sql /root/backup_init.sql
+
+CMD /usr/bin/supervisord -c /etc/supervisord.conf
---
+++ root/bin/start_mysqld.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+function watch_pid {
+   if [ -f ${1} ];then
+      sleep 5
+      watch_pid ${1}
+   else
+      exit 1
+   fi
+
+}
+
+mysql_install_db
+sleep 5
+/usr/bin/mysqld_safe & 
+sleep 5
+
+RANDPW=$(date +"%F_%H-%M-%S"|md5sum|awk '{print $1}')
+MYSQLPW_ROOT=${MYSQLPW_ROOT-rootPw}
+MYSQLPW_ROOT=${MYSQLPW_ROOT-${RANDPW}}
+MYSQLPW_COLL=${MYSQLPW_COLLAB-collab}
+
+mysqladmin -u root password ${MYSQLPW_ROOT}
+mysql -uroot -p${MYSQLPW_ROOT} -e "CREATE DATABASE collabtive"
+mysql -uroot -p${MYSQLPW_ROOT} -e "GRANT index, create, select, insert, update, delete, alter, lock tables ON collabtive.* TO 'collabuser'@'localhost' identified by '${MYSQLPW_COLL}';"
+mysql -uroot -p${MYSQLPW_ROOT} -e "FLUSH PRIVILEGES;"
+
+if [ ! -f /var/www/html/index.php ];then
+   if [ -d /backup ];then
+      echo "# BACKUP directory detected"
+      tar_file=$(find /backup/ -type f -name \*.tar | xargs ls -ltr | tail -n 1|awk '{print $NF}')
+      cd /var/www/html/
+      echo "# tar xf ${tar_file}"
+      tar xf ${tar_file}
+      chown apache: /var/www/html/config/standard/config.php
+      chown apache: -R  /var/www/html/{files,templates_c}
+      sql_file=$(find /backup/ -type f -name \*.sql | xargs ls -ltr | tail -n 1|awk '{print $NF}')
+      echo "# restore from '${sql_file}'"
+      mysql -uroot -p${MYSQLPW_ROOT} < ${sql_file}
+   else
+      echo "# initializing basic"
+      # TODO: DB-PW for collabuser should be adjusted
+      cd /var/www/html/
+      tar xf /root/collabtive20_init.tar
+      mysql -uroot -p${MYSQLPW_ROOT} < /root/backup_init.sql
+   fi
+fi
+
+rm -f /root/backup_init.sql /root/collabtive20_init.tar
+
+watch_pid /var/run/mysqld/mysqld.pid
